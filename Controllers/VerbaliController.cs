1f24de955a56977dafade6b092e6314ee797617e
dimitrilzk <dimitrilazuka95@gmail.com>
2022-12-23 19:06:46 +0100

    bisoga inserire violazione

---
+++ Controllers/VerbaliController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PoliziaDiStato.Controllers
+{
+    public class VerbaliController : Controller
+    {
+        // GET: Verbali
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
